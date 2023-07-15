e2a4160680255c86cf5ff053ad22245c9b220137
ranss@rambler.ru <ranss@rambler.ru>
2023-07-15 13:25:48 +0300

    init commit

---
+++ src/main/java/com/example/homework214/HomeWork214Application.java
@@ -0,0 +1,13 @@
+package com.example.homework214;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork214Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork214Application.class, args);
+    }
+
+}
